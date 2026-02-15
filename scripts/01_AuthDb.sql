a3affdc9dc276e617897cbdc9b8d6ff039c81264
Lucas Curual <lucascurual97@gmail.com>
2026-02-15 16:48:29 -0300

    Propuesta de script para la tabla de usuarios del servicio de autenticación

---
+++ scripts/01_AuthDb.sql
@@ -0,0 +1,23 @@
+-- =========================================
+-- SCRIPT PARA MICROSERVICIO: Auth.API
+-- =========================================
+USE MASTER;
+GO
+
+CREATE DATABASE SG_Financial_AuthDb;
+GO
+
+USE SG_Financial_AuthDb;
+GO
+
+CREATE TABLE Users (
+   Id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
+   UserName VARCHAR(255) NOT NULL UNIQUE,
+   PasswordHash VARCHAR(255) NOT NULL,
+   CreatedAt DATETIME2 NOT NULL DEFAULT GETUTCDATE(),
+   IsActive BIT NOT NULL DEFAULT 1
+);
+GO
+
+CREATE NONCLUSTERED INDEX IX_Users_UserName ON Users(UserName);
+GO
