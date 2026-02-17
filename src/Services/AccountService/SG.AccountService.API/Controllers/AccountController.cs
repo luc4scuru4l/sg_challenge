3f39a958a4e7cb5347aba5c1ed2c04044c1a8989
Lucas Curual <lucascurual97@gmail.com>
2026-02-17 18:04:27 -0300

    Capa API del servicio de cuentas: Se agregan los endpoints para crear la cuenta y para consultar el balance

--- src/Services/AccountService/SG.AccountService.API/Controllers/AccountController.cs
+++ src/Services/AccountService/SG.AccountService.API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
+using System.Security.Claims; 
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SG.AccountService.Application.Interfaces;
+using SG.AccountService.Application.DTOs;
 
 namespace SG.AccountService.API.Controllers;
 
@@ -14,5 +16,33 @@ public class AccountController : ControllerBase
   public AccountController(IAccountService accountService)
   {
     _accountService = accountService;
+  }
+
+  [HttpPost]
+  public async Task<ActionResult<AccountResponseDto>> CreateAccount(CancellationToken cancellationToken)
+  {
+    var userIdClaim = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    if (!Guid.TryParse(userIdClaim, out var userId))
+    {
+      return Unauthorized("Token inválido.");
+    }
+
+    var account = await _accountService.CreateAccountAsync(userId, cancellationToken);
+
+    return CreatedAtAction(nameof(GetBalance), new { id = account.AccountId }, account);
+  }
+
+  [HttpGet("{id:guid}/balance")]
+  public async Task<ActionResult<AccountResponseDto>> GetBalance(Guid id, CancellationToken cancellationToken)
+  {
+    try
+    {
+      var balance = await _accountService.GetBalanceAsync(id, cancellationToken);
+      return Ok(new AccountResponseDto(id, balance));
+    }
+    catch (KeyNotFoundException ex)
+    {
+      return NotFound(ex.Message);
+    }
   }
 }
