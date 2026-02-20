e9a16986341e3fcc45cd8da8c7b255f991a56a1e
Lucas Curual <lucascurual97@gmail.com>
2026-02-19 22:16:57 -0300

    Capa de dominio del serv de auth: Fix, se me pasaron dos setters publicos

--- src/Services/AuthService/SG.AuthService.Domain/Entities/User.cs
+++ src/Services/AuthService/SG.AuthService.Domain/Entities/User.cs
@@ -3,9 +3,9 @@ namespace SG.AuthService.Domain.Entities;
 public class User
 {
   public Guid Id { get; private set; }
-  public string UserName { get; set; }
+  public string UserName { get; private set; }
   public string PasswordHash { get; private set; }
-  public bool IsActive { get; set; }
+  public bool IsActive { get; private set; }
   public DateTime CreatedAt { get; private set; }
   
   protected User()
