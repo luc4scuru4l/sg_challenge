7bb1677e0c5bdd598495bb835399dd2a63c1ab05
luc4scuru4l <lucascurual97@gmail.com>
2026-02-20 18:42:35 +0000

    Se aplica formateo

--- src/Services/AccountService/tests/SG.AccountService.UnitTests/Application/Services/AccountServiceTests.cs
+++ src/Services/AccountService/tests/SG.AccountService.UnitTests/Application/Services/AccountServiceTests.cs
@@ -4,70 +4,69 @@ using SG.AccountService.Application.DTOs;
 using SG.AccountService.Application.Exceptions;
 using SG.AccountService.Application.Interfaces;
 using AccountServiceClass = SG.AccountService.Application.Services.AccountService;
-
 using SG.AccountService.Domain.Entities;
 
 namespace SG.AccountService.UnitTests.Application.Services;
 
 public class AccountServiceTests
 {
-    private readonly Mock<IAccountRepository> _accountRepositoryMock;
-    private readonly AccountServiceClass _sut;
+  private readonly Mock<IAccountRepository> _accountRepositoryMock;
+  private readonly AccountServiceClass _sut;
 
-    public AccountServiceTests()
-    {
-        _accountRepositoryMock = new Mock<IAccountRepository>();
-        _sut = new AccountServiceClass(_accountRepositoryMock.Object);
-    }
+  public AccountServiceTests()
+  {
+    _accountRepositoryMock = new Mock<IAccountRepository>();
+    _sut = new AccountServiceClass(_accountRepositoryMock.Object);
+  }
 
-    [Fact]
-    public async Task GetBalanceAsync_WhenAccountExistsAndBelongsToUser_ShouldReturnBalance()
-    {
-        // Arrange (Preparar)
-        var accountId = Guid.NewGuid();
-        var requestUserId = Guid.NewGuid();
-        var expectedBalance = 15000.50m;
-        
-        // Simulamos una cuenta válida creada por el usuario
-        var fakeAccount = new Account(requestUserId);
-        fakeAccount.Deposit(expectedBalance);
-        
-        _accountRepositoryMock
-            .Setup(repo => repo.GetByIdAndUserIdAsync(accountId, requestUserId, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(fakeAccount);
+  [Fact]
+  public async Task GetBalanceAsync_WhenAccountExistsAndBelongsToUser_ShouldReturnBalance()
+  {
+    // Arrange (Preparar)
+    var accountId = Guid.NewGuid();
+    var requestUserId = Guid.NewGuid();
+    var expectedBalance = 15000.50m;
 
-        // Act (Actuar)
-        var result = await _sut.GetBalanceAsync(accountId, requestUserId, CancellationToken.None);
+    // Simulamos una cuenta válida creada por el usuario
+    var fakeAccount = new Account(requestUserId);
+    fakeAccount.Deposit(expectedBalance);
 
-        // Assert (Afirmar)
-        result.Should().NotBeNull();
-        result.Should().BeOfType<AccountResponseDto>();
-        result.AccountId.Should().Be(fakeAccount.Id);
-        result.Balance.Should().Be(expectedBalance);
-        
-        // Verificamos que el servicio efectivamente llamó a la base de datos exactamente 1 vez
-        _accountRepositoryMock.Verify(repo => 
-            repo.GetByIdAndUserIdAsync(accountId, requestUserId, It.IsAny<CancellationToken>()), 
-            Times.Once);
-    }
+    _accountRepositoryMock
+      .Setup(repo => repo.GetByIdAndUserIdAsync(accountId, requestUserId, It.IsAny<CancellationToken>()))
+      .ReturnsAsync(fakeAccount);
 
-    [Fact]
-    public async Task GetBalanceAsync_WhenAccountDoesNotExistOrBelongsToAnotherUser_ShouldThrowNotFoundException()
-    {
-        // Arrange
-        var accountId = Guid.NewGuid();
-        var hackerUserId = Guid.NewGuid();
+    // Act (Actuar)
+    var result = await _sut.GetBalanceAsync(accountId, requestUserId, CancellationToken.None);
 
-        // Le decimos al Mock: "Devolvé NULL simulando que el filtro SQL no encontró coincidencias"
-        _accountRepositoryMock
-            .Setup(repo => repo.GetByIdAndUserIdAsync(accountId, hackerUserId, It.IsAny<CancellationToken>()))
-            .ReturnsAsync((Account)null!);
+    // Assert (Afirmar)
+    result.Should().NotBeNull();
+    result.Should().BeOfType<AccountResponseDto>();
+    result.AccountId.Should().Be(fakeAccount.Id);
+    result.Balance.Should().Be(expectedBalance);
 
-        // Act
-        Func<Task> act = async () => await _sut.GetBalanceAsync(accountId, hackerUserId, CancellationToken.None);
+    // Verificamos que el servicio efectivamente llamó a la base de datos exactamente 1 vez
+    _accountRepositoryMock.Verify(repo =>
+        repo.GetByIdAndUserIdAsync(accountId, requestUserId, It.IsAny<CancellationToken>()),
+      Times.Once);
+  }
 
-        // Assert
-        await act.Should().ThrowAsync<AccountNotFoundException>()
-                 .WithMessage($"No se encontró la cuenta con el ID especificado: {accountId}");
-    }
+  [Fact]
+  public async Task GetBalanceAsync_WhenAccountDoesNotExistOrBelongsToAnotherUser_ShouldThrowNotFoundException()
+  {
+    // Arrange
+    var accountId = Guid.NewGuid();
+    var hackerUserId = Guid.NewGuid();
+
+    // Le decimos al Mock: "Devolvé NULL simulando que el filtro SQL no encontró coincidencias"
+    _accountRepositoryMock
+      .Setup(repo => repo.GetByIdAndUserIdAsync(accountId, hackerUserId, It.IsAny<CancellationToken>()))
+      .ReturnsAsync((Account)null!);
+
+    // Act
+    Func<Task> act = async () => await _sut.GetBalanceAsync(accountId, hackerUserId, CancellationToken.None);
+
+    // Assert
+    await act.Should().ThrowAsync<AccountNotFoundException>()
+      .WithMessage($"No se encontró la cuenta con el ID especificado: {accountId}");
+  }
 }
